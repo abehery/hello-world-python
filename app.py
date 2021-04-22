8ddb6dcb220081f430d43edd0ed9349d496b7be8
abehery <a.mhbehery@gmail.com>
2021-04-22 04:52:45 +0200

    test1 msg

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 START = time.time()
-
+asasas
 def elapsed():
     running = time.time() - START
     minutes, seconds = divmod(running, 60)
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World (Python)! (up %s)\n" % elapsed()
+    return "Hello World  with all(Python)! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
